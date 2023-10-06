f6aa2c7ba9e43636ac78a0d87fab89e97c8f7d1a
Juverio <juverio@gmail.com>
2023-10-07 00:36:32 +0700

    Login Page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,93 +1,172 @@
 import 'package:crowdfunding/screens/base_screen.dart';
 import 'package:crowdfunding/screens/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> onLoginButtonPressed(BuildContext context) async {
+    final email = emailController.text;
+    final password = passwordController.text;
+
+    try {
+      final userSnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .where('email', isEqualTo: email)
+          .get();
+
+      if (userSnapshot.docs.isNotEmpty) {
+        final userData = userSnapshot.docs.first.data() as Map<String, dynamic>;
+        final storedPassword = userData['password'] as String;
+        if (storedPassword == password) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const BasePage(),
+            ),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Invalid email or password.'),
+              duration: Duration(milliseconds: 2000),
+            ),
+          );
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('User not found.'),
+            duration: Duration(milliseconds: 2000),
+          ),
+        );
+      }
+    } catch (e) {
+      print('Error: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('An error occurred. Please try again.'),
+          duration: Duration(milliseconds: 2000),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-        centerTitle: true,
-        backgroundColor: const Color.fromRGBO(129, 199, 132, 1),
-        automaticallyImplyLeading: false,
-      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset('images/Logo.png'),
-            const Text('halo'),
-            // Create login page
-            SizedBox(
-              width: 250,
-              height: 40,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color.fromRGBO(129, 199, 132, 1),
-                  ),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                  shape: MaterialStateProperty.all<OutlinedBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const BasePage(),
-                    ),
-                  );
-                },
-                child: const Text('Login'),
+            Image.asset(
+              "images/Logo.png",
+            ),
+            Container(
+              width: 315,
+              height: 310,
+              decoration: BoxDecoration(
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(10),
               ),
-            ),
-            const Padding(padding: EdgeInsets.all(5)),
-            const Text(
-              'Not Registered?',
-              style: TextStyle(color: Colors.black),
-            ),
-            const Padding(padding: EdgeInsets.all(5)),
-            // Button to register page
-            SizedBox(
-              width: 250,
-              height: 40,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                  foregroundColor: MaterialStateProperty.all<Color>(
-                      const Color.fromRGBO(129, 199, 132, 1)),
-                  shape: MaterialStateProperty.all<OutlinedBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      side: const BorderSide(
-                        color: Color.fromRGBO(129, 199, 132, 1),
-                        width: 1,
+              child: Center(
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 20.0, bottom: 10),
+                      child: Text(
+                        "Login", // Updated text to "Login"
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const RegisterPage(),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: SizedBox(
+                        width: 242,
+                        height: 56,
+                        child: TextField(
+                          controller: emailController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Email',
+                          ),
+                        ),
+                      ),
                     ),
-                  );
-                },
-                child: const Text('Register'),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0, bottom: 10.0),
+                      child: SizedBox(
+                        width: 242,
+                        height: 56,
+                        child: TextField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Password',
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 242,
+                      height: 56,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromRGBO(129, 199, 132, 1),
+                        ),
+                        onPressed: () => onLoginButtonPressed(context),
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text("Not registered?"),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const RegisterPage(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "Register",
+                            style: TextStyle(
+                              color: Color.fromRGBO(129, 199, 132, 1),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
